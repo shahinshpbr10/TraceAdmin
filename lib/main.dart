35788c02c017172abfc58cc359f073237a2b74cf
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2025-04-05 09:44:25 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart'; // Add this import
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
-
-
